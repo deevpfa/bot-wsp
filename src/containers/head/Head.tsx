4b7a671317fcb609c8cbbc69f29ce3e9e5c2c14d
crowdium <admin@crowdium.com>
2024-07-16 12:19:56 -0300

    add script fb

--- src/containers/head/Head.tsx
+++ src/containers/head/Head.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { FC } from "react";
+import Script from "next/script";
 
 export const HeadComponent: FC<{ title: string | null }> = ({ title }) => {
     return (
@@ -10,6 +11,20 @@ export const HeadComponent: FC<{ title: string | null }> = ({ title }) => {
                 <link rel="shortcut icon" href='images/IsoSinFondo.png' />
                 <meta name="description" content="" />
                 <meta name="facebook-domain-verification" content="upnahiawmm21w0qd74rflochd2h7sq" />
+                <Script>
+                    window.fbAsyncInit = function() {
+                        //@ts-ignore 
+                        FB.init({
+                            appId: '3935684223377746',
+                            autoLogAppEvents: true,
+                            xfbml: true,
+                            version: 'v20.0'
+                        })
+                    };
+                </Script>
+                <Script async defer crossOrigin="anonymous"
+                    src="https://connect.facebook.net/en_US/sdk.js">
+                </Script>
             </Head>
         </>
     )
