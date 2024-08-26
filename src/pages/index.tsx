dc596e880b3f803369a06f529f042d230707f713
crowdium <admin@crowdium.com>
2024-08-26 18:02:40 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       {/* <div className='relative bg-secondary-darken pb-12'> */}
       <Chat />
 
-      <div className='md:px-16  flex flex-col gap-y-16'>
+      <div className='md:px-16 z-10 relative flex flex-col gap-y-16'>
         <section className="relative  bg-cover lg:bg-contain overflow-hidden">
           <Header />
           <Main />
